1ded99291dd841325b4111952c8a58736ba0424c
Mitch-Kenward <mkenward29@gmail.com>
2021-03-30 03:46:26 +0000

    the tail end of an array

---
+++ tail.js
@@ -0,0 +1,24 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(2, 1);
+
+const tail = function(words) {
+  return words.slice(1);
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
