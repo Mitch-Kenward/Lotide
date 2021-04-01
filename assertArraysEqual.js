16a452cff83d57bd96f2764bdb59ed0f8e09815c
Mitch-Kenward <mkenward29@gmail.com>
2021-04-01 02:24:15 +0000

    assertion for arrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,28 @@
 const assertArraysEqual = function(arr1,arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const eqArrays = function(arr1,arr2) {
   if (arr1.length !== arr2.length) {
-    console.log(`❌Assertion Failed: ${arr1.length} !== ${arr2.length}`);
+    return false;
   }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log(`❌Assertion Failed: ${arr1[i]} !== ${arr2[i]}`);
+      return false;
     }
   }
-  console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
+  return true;
 };
+
+
+assertArraysEqual(
+  [1, 2, 3], [1, 2, 3])// => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1])// => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+
