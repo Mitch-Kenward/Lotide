e28662ee355395e00b34c125b589e25798733ee5
Mitch-Kenward <mkenward29@gmail.com>
2021-04-02 01:26:40 +0000

    assertion for objects

---
+++ assertObjectEqual.js
@@ -0,0 +1,28 @@
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (const key of Object.keys(object1)) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key],object2[key])) {
+        return false;     
+      }
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  
+  const inspect = require('util').inspect;
+  if (eqObjects(actual === expected)) {
+    console.log(`✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  }
+  if (actual !== expected) {
+    console.log(`❌Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+
+
