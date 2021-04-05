1c32c5eb4e0dacdf9a0fb5bbeb76dd5dbb25c2ce
Mitch-Kenward <mkenward29@gmail.com>
2021-04-05 20:48:59 +0000

    refactoring assert function

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,13 @@
 // FUNCTION IMPLEMENTATION
-const assertArraysEqual = function(actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   }
   if (actual !== expected) {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-assertArraysEqual("Lighthouse Labs", "Bootcamp");
-assertArraysEqual(1, 1);
-assertArraysEqual("Bootcamp", "Bootcamp");
-assertArraysEqual(2, 1);
+
+
+module.exports = assertEqual;
