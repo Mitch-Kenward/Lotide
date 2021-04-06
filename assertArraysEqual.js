3b8d18d0fec95e92fefdbc87d70e7c8e0394352e
Mitch-Kenward <mkenward29@gmail.com>
2021-04-06 02:54:53 +0000

    refactoring

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,8 @@
 const assertArraysEqual = function(arr1,arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
-    console.log(`❌Assertion Failed: ${arr1} !== ${arr2}`);
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
 
@@ -19,10 +19,6 @@ const eqArrays = function(arr1,arr2) {
 };
 
 
-assertArraysEqual(
-  [1, 2, 3], [1, 2, 3])// => should PASS
-assertArraysEqual([1, 2, 3], [3, 2, 1])// => false
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+module.exports = assertArraysEqual;
+module.exports = eqArrays;
 
